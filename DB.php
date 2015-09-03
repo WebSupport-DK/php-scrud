b19daff32fc15037e9ce8ac4dfa2f4383766b208
thom855j <thom855j@cvkweb.dk>
2015-09-03 15:56:35 +0200

    added option to alter table

--- DB.php
+++ DB.php
@@ -1,60 +1,56 @@
 <?php
 
-namespace thom855j\sql ;
+namespace thom855j\sql;
 
-use PDO ;
+use PDO;
 
 class DB
 {
 
     protected static
-            $_instance = null ;
+            $_instance = null;
     private
-            $type ,
-            $server ,
-            $database ,
-            $user ,
-            $password ;
+            $type,
+            $server,
+            $database,
+            $user,
+            $password;
     public
-            $_pdo ,
-            $_query ,
-            $_error              = false ,
-            $_results ,
-            $_first ,
-            $_count              = 0 ;
+            $_pdo,
+            $_query,
+            $_error              = false,
+            $_results,
+            $_first,
+            $_count              = 0;
 
     public
-            function __construct( $type , $server , $database , $user ,
-                                  $password )
+            function __construct($type, $server, $database, $user, $password)
     {
-        $this->type     = $type ;
-        $this->server   = $server ;
-        $this->database = $database ;
-        $this->user     = $user ;
-        $this->password = $password ;
-        
+        $this->type     = $type;
+        $this->server   = $server;
+        $this->database = $database;
+        $this->user     = $user;
+        $this->password = $password;
+
         try
         {
-            $this->_pdo = new PDO( $this->type . ':host=' . $this->server . ';dbname=' . $this->database ,
-                                   $this->user , $this->password ) ;
-            $this->_pdo->exec( 'set names utf8' ) ;
+            $this->_pdo = new PDO($this->type . ':host=' . $this->server . ';dbname=' . $this->database, $this->user, $this->password);
+            $this->_pdo->exec('set names utf8');
         }
-        catch ( PDOException $e )
+        catch (PDOException $e)
         {
-            die( $e->getMessage() ) ;
+            die($e->getMessage());
         }
     }
 
     public static
-            function load( $type = null , $server = null , $database = null ,
-                           $user = null , $password = null )
+            function load($type = null, $server = null, $database = null, $user = null, $password = null)
     {
-        if ( !isset( self::$_instance ) )
+        if (!isset(self::$_instance))
         {
-            self::$_instance = new DB( $type , $server , $database , $user ,
-                                       $password ) ;
+            self::$_instance = new DB($type, $server, $database, $user, $password);
         }
-        return self::$_instance ;
+        return self::$_instance;
     }
 
     public
@@ -67,39 +63,39 @@ class DB
             function lastInsertId()
     {
         // Return last inserted ID from DB 
-        return $this->_pdo->lastInsertId() ;
+        return $this->_pdo->lastInsertId();
     }
 
     public
-            function query( $sql , $params = array() )
+            function query($sql, $params = array())
     {
-        $this->_error = false ;
-        $prepare      = $this->_query = $this->_pdo->prepare( $sql ) ;
+        $this->_error = false;
+        $prepare      = $this->_query = $this->_pdo->prepare($sql);
 
-        if ( isset( $prepare ) )
+        if (isset($prepare))
         {
 
-            $x = 1 ;
-            if ( count( $params ) )
+            $x = 1;
+            if (count($params))
             {
-                foreach ( $params as $param )
+                foreach ($params as $param)
                 {
-                    $this->_query->bindValue( $x , $param ) ;
-                    $x++ ;
+                    $this->_query->bindValue($x, $param);
+                    $x++;
                 }
             }
 
-            if ( $this->_query->execute() )
+            if ($this->_query->execute())
             {
-                $this->_results = $this->_query->fetchAll( PDO::FETCH_OBJ ) ;
-                $this->_count   = $this->_query->rowCount() ;
+                $this->_results = $this->_query->fetchAll(PDO::FETCH_OBJ);
+                $this->_count   = $this->_query->rowCount();
             }
             else
             {
-                $this->_error = true ;
+                $this->_error = true;
             }
         }
-        return ( object ) $this ;
+        return (object) $this;
     }
 
     /**
@@ -112,180 +108,183 @@ class DB
      * @return boolean|object
      */
     public
-            function action( $action , $table , $where = array() ,
-                             $options = array() )
+            function action($action, $table, $where = array(), $options = array())
     {
-        $sql   = "{$action} FROM {$table}" ;
-        $value = array() ;
+        $sql   = "{$action} FROM {$table}";
+        $value = array();
 
-        if ( !empty( $where ) )
+        if (!empty($where))
         {
-            $sql .= " WHERE " ;
-            foreach ( $where as $clause )
+            $sql .= " WHERE ";
+            foreach ($where as $clause)
             {
-                if ( count( $clause ) === 3 )
+                if (count($clause) === 3)
                 {
-                    $operators = array( '=' , '>' , '<' , ' >=' , '<=' ) ;
+                    $operators = array('=', '>', '<', ' >=', '<=');
 
-                    if ( isset( $clause ) )
+                    if (isset($clause))
                     {
-                        $field     = $clause[ 0 ] ;
-                        $operator  = $clause[ 1 ] ;
-                        $value[]   = $clause[ 2 ] ;
-                        $bindValue = '?' ;
+                        $field     = $clause[0];
+                        $operator  = $clause[1];
+                        $value[]   = $clause[2];
+                        $bindValue = '?';
                     }
 
-                    if ( in_array( $operator , $operators ) )
+                    if (in_array($operator, $operators))
                     {
-                        $sql .= "{$field} {$operator} {$bindValue}" ;
-                        $sql .= " AND " ;
+                        $sql .= "{$field} {$operator} {$bindValue}";
+                        $sql .= " AND ";
                     }
                 }
                 //var_dump($sql);
             }
-            $sql = rtrim( $sql , " AND " ) ;
+            $sql = rtrim($sql, " AND ");
         }
 
-        if ( !empty( $options ) )
+        if (!empty($options))
         {
-            foreach ( $options as $optionKey => $optionValue )
+            foreach ($options as $optionKey => $optionValue)
             {
-                $sql .= " {$optionKey} {$optionValue}" ;
+                $sql .= " {$optionKey} {$optionValue}";
             }
         }
 
-        if ( !$this->query( $sql , $value )->error() )
+        if (!$this->query($sql, $value)->error())
         {
-            return ( object ) $this ;
+            return (object) $this;
         }
-        return false ;
+        return false;
     }
 
     public
-            function select( $select = array() , $table , $where = array() ,
-                             $options = array() )
+            function select($select = array(), $table, $where = array(), $options = array())
     {
-        return $this->action( 'SELECT ' . implode( $select , ', ' ) , $table ,
-                                                   $where , $options ) ;
+        return $this->action('SELECT ' . implode($select, ', '), $table, $where, $options);
     }
 
     public
-            function search( $table , $rows , $searchQuery ,
-                             $searchTerms = array() , $options = null )
+            function search($table, $rows, $searchQuery, $searchTerms = array(), $options = null)
     {
 
 
-        if ( !empty( $searchQuery ) && !empty( $searchTerms ) )
+        if (!empty($searchQuery) && !empty($searchTerms))
         {
-            $z = 1 ;
-            for ( $x = 0 ; $x < $searchTerms ; $x++ )
+            $z = 1;
+            for ($x = 0; $x < $searchTerms; $x++)
             {
-                for ( $y = 0 ; $y < count( $searchQuery ) ; $y++ )
+                for ($y = 0; $y < count($searchQuery); $y++)
                 {
-                    $this->_query->bindValue( $z++ , $searchQuery[ $y ] ) ;
+                    $this->_query->bindValue($z++, $searchQuery[$y]);
                 }
             }
         }
 
-        $query = "" ;
-        if ( isset( $searchQuery ) )
+        $query = "";
+        if (isset($searchQuery))
         {
-            foreach ( $searchTerms as $term )
+            foreach ($searchTerms as $term)
             {
-                $query .= "{$term} LIKE '%{$searchQuery}%' OR " ;
+                $query .= "{$term} LIKE '%{$searchQuery}%' OR ";
             }
 
-            $search = trim( $query , "OR " ) ;
+            $search = trim($query, "OR ");
 
-            $sql = "SELECT {$rows} FROM {$table} WHERE {$search}" ;
+            $sql = "SELECT {$rows} FROM {$table} WHERE {$search}";
 
-            if ( !$this->query( $sql )->error() )
+            if (!$this->query($sql)->error())
             {
-                return ( object ) $this ;
+                return (object) $this;
             }
         }
     }
 
     public
-            function insert( $table , $fields = array() )
+            function insert($table, $fields = array())
     {
-        $keys   = array_keys( $fields ) ;
-        $values = '' ;
-        $x      = 1 ;
+        $keys   = array_keys($fields);
+        $values = '';
+        $x      = 1;
 
-        foreach ( $fields as $field )
+        foreach ($fields as $field)
         {
-            $values .= '?' ;
-            if ( $x < count( $fields ) )
+            $values .= '?';
+            if ($x < count($fields))
             {
-                $values .= ', ' ;
+                $values .= ', ';
             }
-            $x++ ;
+            $x++;
         }
 
-        $sql = "INSERT INTO {$table} (`" . implode( '`,`' , $keys ) . "`) VALUES ({$values})" ;
+        $sql = "INSERT INTO {$table} (`" . implode('`,`', $keys) . "`) VALUES ({$values})";
 
-        if ( !$this->query( $sql , $fields )->error() )
+        if (!$this->query($sql, $fields)->error())
         {
-            return true ;
+            return true;
         }
-        return false ;
+        return false;
     }
 
     public
-            function delete( $table , $where = array() )
+            function delete($table, $where = array())
     {
-        return $this->action( 'DELETE' , $table , $where ) ;
+        return $this->action('DELETE', $table, $where);
     }
 
     public
-            function update( $table , $column_ID , $ID , $fields = array() )
+            function update($table, $column_ID, $ID, $fields = array())
     {
-        $set = '' ;
-        $x   = 1 ;
+        $set = '';
+        $x   = 1;
 
-        foreach ( $fields as $name => $value )
+        foreach ($fields as $name => $value)
         {
-            $set .= "{$name} = ?" ;
-            if ( $x < count( $fields ) )
+            $set .= "{$name} = ?";
+            if ($x < count($fields))
             {
-                $set .= ', ' ;
+                $set .= ', ';
             }
-            $x++ ;
+            $x++;
         }
 
-        $sql = "UPDATE {$table} SET {$set} WHERE {$column_ID} = {$ID}" ;
+        $sql = "UPDATE {$table} SET {$set} WHERE {$column_ID} = {$ID}";
 
-        if ( !$this->query( $sql , $fields )->error() )
+        if (!$this->query($sql, $fields)->error())
         {
-            return true ;
+            return true;
         }
-        return false ;
+        return false;
+    }
+
+    public
+            function alter($table, $column, $info)
+    {
+        $sql = "ALTER TABLE ? ADD ? ?";
+        return $this->query($sql, array($table,$column,$info));
     }
 
     public
             function results()
     {
-        return $this->_results ;
+        return $this->_results;
     }
 
     public
             function first()
     {
-        $this->_first = $this->results() ;
-        return $this->_first[ 0 ] ;
+        $this->_first = $this->results();
+        return $this->_first[0];
     }
 
     public
             function error()
     {
-        return $this->_error ;
+        return $this->_error;
     }
 
     public
             function count()
     {
-        return $this->_count ;
+        return $this->_count;
     }
 
 }
